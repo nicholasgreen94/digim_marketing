77b3619bcd9b4d58f791604eb0d87713e9df64c1
Nicholas Green <nicholasgreen94@gmail.com>
2016-05-07 18:23:07 -0700

    Working with buttons when clicked add more content

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ $(function() {
       $(".main-content-add").slideDown();
     });
     
+    $(".team-members p").hide();
+    $(".team-members").on("mouseover click", function() {
+      $(".team-members p").toggle();
+    })
+    
+    $(".team-members").on("mouseout", function() {
+      $(".team-members p").hide();
+    });
+    
     var date = new Date();
     $("#year").html(date.getFullYear());
+    
+    $(".graphic-work").hide();
+    $(".graphic-btn").on("click", function() {
+      $(".graphic-work").slideToggle("1500");
+    });
 })
